18a812eda854a3769c96a7006732e04e79a34631
Grace <q0sul3sul3@gmail.com>
2024-01-26 15:00:34 +0800

    Refactor to sort_xml function

--- run.py
+++ run.py
@@ -2,19 +2,29 @@ from collections import OrderedDict
 
 import xmltodict
 
-with open('data.xml', 'r') as xml_file:
-    xml_data = xml_file.read()
 
-data = xmltodict.parse(xml_data, dict_constructor=OrderedDict)
+def sort_xml(xml_file_path):
+    with open(xml_file_path, 'r') as xml_file:
+        xml_data = xml_file.read()
 
-sorted_recs = sorted(data['RECS']['REC'], key=lambda x: x['MB']['MB1']['M03'])
-for rec in sorted_recs:
-    if 'MB2' in rec['MB'] and isinstance(rec['MB']['MB2'], list):
-        rec['MB']['MB2'] = sorted(rec['MB']['MB2'], key=lambda x: x['D06'])
+    data = xmltodict.parse(xml_data, dict_constructor=OrderedDict)
 
-sorted_dict = OrderedDict()
-sorted_dict['RECS'] = OrderedDict()
-sorted_dict['RECS']['REC'] = sorted_recs
+    sorted_recs = sorted(data['RECS']['REC'], key=lambda x: x['MB']['MB1']['M03'])
+    for rec in sorted_recs:
+        if 'MB2' in rec['MB'] and isinstance(rec['MB']['MB2'], list):
+            rec['MB']['MB2'] = sorted(rec['MB']['MB2'], key=lambda x: x['D06'])
 
-with open('data_sorted.xml', 'w', encoding='Big5') as sorted_xml_file:
-    sorted_xml_file.write(xmltodict.unparse(sorted_dict, pretty=True, encoding='Big5'))
+    sorted_dict = OrderedDict()
+    sorted_dict['RECS'] = OrderedDict()
+    sorted_dict['RECS']['REC'] = sorted_recs
+
+    sorted_file_path = xml_file_path.replace('.xml', '_sorted.xml')
+    with open(sorted_file_path, 'w', encoding='Big5') as sorted_xml_file:
+        sorted_xml_file.write(
+            xmltodict.unparse(sorted_dict, pretty=True, encoding='Big5')
+        )
+
+
+if __name__ == '__main__':
+    xml_file_path = 'data.xml'
+    sort_xml(xml_file_path)
